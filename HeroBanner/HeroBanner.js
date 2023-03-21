430feb1328c53a68139bcc7003df77d64689bc98
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-03-21 23:22:34 +0100

    Hero Banner Component erstellt mit css

---
+++ HeroBanner/HeroBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function HeroBanner(props) {
+    const {background,height}=props
+return (
+    <>
+        <div style={{backgroundImage:`url(${background})`}} className="hero-banner" >
+        
+        </div>
+    </>
+)}
+
+export default HeroBanner
